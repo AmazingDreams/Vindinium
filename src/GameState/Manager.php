f96472501dd251238d71018eb455db62fc054366
Dennis Ruhe <dennis@amazingsystems.nl>
2014-11-12 11:25:47 +0100

    Added startup code

---
+++ src/GameState/Manager.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace AD9001\GameState;
+
+class Manager {
+
+	public function go($args)
+	{
+
+	}
+
+}
--- src/cli.php
+++ src/cli.php
@@ -1,3 +1,12 @@
 <?php
 
 include __DIR__.'/../vendor/autoload.php';
+
+// We draaien deze shit op dit bestandje
+// 1. Check of we op de test omgeving of de arena moeten gaan
+// 2. Haal de game op
+// 3. Verdeel de info over de classes en shit
+// 4. Begin de game enzo
+
+$manager = new \AD9001\GameState\Manager();
+$manager->go($argv);
